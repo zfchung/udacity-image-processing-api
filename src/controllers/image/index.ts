d755069e3dc7e29e01f65cb3b89726548d961b48
ZF Chung (Zack) <zfchung1@gmail.com>
2022-10-01 02:15:06 +0800

    extracted getImage() service

--- src/controllers/image/index.ts
+++ src/controllers/image/index.ts
@@ -1,5 +1,7 @@
 import {Response, Request} from "express";
+import {getImage} from "../../services/image";
 
 export function imageController(req: Request, res: Response) {
-    res.send("Image Controller");
+    const result = getImage();
+    res.send(result);
 }
