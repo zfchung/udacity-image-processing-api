f8c3a317bbc65f60444044405e4ee9e03664ee44
ZF Chung (Zack) <zfchung1@gmail.com>
2022-10-01 05:01:33 +0800

    added test for services/image

---
+++ src/tests/services/image/indexSpec.ts
@@ -0,0 +1,12 @@
+import {getImage} from "../../../services/image";
+
+describe("getImage", () => {
+    it("should return path of file", async() => {
+        const query = {
+            fileName: "fjord",
+            width: 1,
+            height: 1
+        }
+        expect(await getImage(query)).toBe("thumb/fjord.jpg");
+    })
+})
