13ee7616a4cd449dd83fa66f708db5cc7da2a720
ZF Chung (Zack) <zfchung1@gmail.com>
2022-10-01 01:47:01 +0800

    added express

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-export function sampleFunction() {
-    return null;
-}
+import express from 'express';
+
+const app = express();
+const defaultPortNumber = 3000;
+const port = Number(process.env.PORT || defaultPortNumber);
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+
+// start the Express server
+app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}`);
+});
+
--- src/tests/indexSpec.ts
+++
@@ -1,8 +0,0 @@
-import {sampleFunction} from "../index";
-
-describe("test sample function", () => {
-    it("should exist", () => {
-        expect(sampleFunction).toBeDefined();
-        expect(sampleFunction()).toBe(null);
-    });
-})
